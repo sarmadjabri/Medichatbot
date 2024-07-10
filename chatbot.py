443868110188ba45e64bf5cb6ba5c0e06be642e0
Sarmad Jabri <114747536+sarmadjabri@users.noreply.github.com>
2024-07-10 09:42:35 -0400

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -5,8 +5,8 @@ from tensorflow.keras.models import load_model
 import numpy as np
 import pickle
 
-# Load disease database and model
-diseases = json.load(open("diseases.JSN"))
+# Load disease database and za model
+diseases = json.load(open("diseases.json"))
 model = load_model("disease_model.h5")
 
 # Load words list
@@ -16,37 +16,41 @@ with open("words.pkl", "rb") as f:
 # Initialize lemmatize
 lemmatizer = WordNetLemmatizer()
 
-# Define a function to process user input
+# Download stuff for za NLTK
+nltk.download('punkt')
+nltk.download('wordnet')
+
+# user input
 def process_symptoms(text):
     # Tokenize and lemmatize user input
     sentence_words = nltk.word_tokenize(text)
     sentence_words = [lemmatizer.lemmatize(word) for word in sentence_words]
 
-    # Convert user input into a numerical representation
+    # Convert input into a numerical presentation
     bag = [0] * len(words)
     for w in sentence_words:
         for i, word in enumerate(words):
             if word == w:
                 bag[i] = 1
 
-    # Use machine learning model to predict possible diseases
+    # Prediction
     bow = np.array([bag])
     res = model.predict(bow)[0]
     ERROR_THRESHOLD = 0.25
     results = [[i, r] for i, r in enumerate(res) if r > ERROR_THRESHOLD]
     results.sort(key=lambda x: x[1], reverse=True)
 
-    # Return a list of possible diseases
+    # Return list(diseases)
     return_list = []
     for r in results:
         return_list.append({"disease": diseases[r[0]]["name"], "probability": str(r[1])})
     return return_list
 
-# Main chatbot loop
+# Loop for bot
 while True:
     print("Enter your symptoms:")
     text = input()
     possible_diseases = process_symptoms(text)
-    print("You might have:")
+    print("It is possible you have:")
     for disease in possible_diseases:
         print(f"{disease['disease']} with a probability of {disease['probability']}")
